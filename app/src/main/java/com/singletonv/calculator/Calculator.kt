b43c3244502e68cc4634b89dd8473b0c18c6f499
SingletonVD <singletonvd@mail.ru>
2025-10-15 22:41:08 +0300

    feat: clear screen on AC button

--- app/src/main/java/com/singletonv/calculator/Calculator.kt
+++ app/src/main/java/com/singletonv/calculator/Calculator.kt
@@ -1,6 +1,8 @@
 package com.singletonv.calculator
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,13 +10,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +24,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+var expression = mutableStateOf("45x8")
+var result = mutableStateOf("360")
 
 @Composable
 fun Calculator(
@@ -49,13 +54,13 @@ fun Calculator(
             horizontalAlignment = Alignment.End
         ) {
             Text(
-                text = "45x8",
+                text = expression.value,
                 fontWeight = FontWeight.SemiBold,
                 fontSize = 36.sp,
                 color = MaterialTheme.colorScheme.onPrimaryContainer
             )
             Text(
-                text = "360",
+                text = result.value,
                 fontWeight = FontWeight.SemiBold,
                 fontSize = 17.sp,
                 color = MaterialTheme.colorScheme.onSecondaryContainer
@@ -109,6 +114,11 @@ fun Calculator(
                 modifier = Modifier
                     .weight(1f)
                     .clip(CircleShape)
+                    .clickable {
+                        Log.d("Calculator", "The 'AC' button is clicked")
+                        expression.value = ""
+                        result.value = ""
+                    }
                     .aspectRatio(1f)
                     .background(MaterialTheme.colorScheme.secondary),
                 contentAlignment = Alignment.Center
